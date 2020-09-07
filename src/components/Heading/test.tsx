bac02fa153ce41645dac34c0e90cbcec85ce0465
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2020-09-07 19:46:01 -0300

    criando alguns components

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,42 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
+import 'jest-styled-components'
+
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'color',
+      '#FAFAFA'
+    )
+  })
+
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'color',
+      '#030517'
+    )
+  })
+
+  it('should render a black heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-left',
+      '0.7rem solid #3CD3C1'
+    )
+  })
+
+  it('should render a black heading with a line to the bottom side', () => {
+    renderWithTheme(<Heading lineBottom>Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+})
